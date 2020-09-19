ba4c4260d80151f37181b3b8777317a6ade5e00a
darszm <bomba.ddax@gmail.com>
2020-09-19 21:33:18 +0200

    adding basic unit tests

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 To run a project use `node index.js`
+
+Tests:
+* run all tests: `npm test`
+* watch mode: `npm test -- --watch`
+* verify coverage: `npm test -- --coverage`
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
-const cookies = require('./lib/cookies');
+const handlers = require('./lib/handlers');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -32,43 +32,26 @@ app.use(express.static(__dirname + '/public'));
 // ROUTES DEFINITIONS
 /**
  * order of routes definition (and middleware) is important! i.e.
- * app.get('/about*', () => { ... }); - usage of wildcard *
- * app.get('/about/contact', () => { ... }); - it will never match
- * app.get('/about/other', () => { ... }); - it will never match
+ * if we are using wildcard *
+ * app.get('/about*', () => { ... }); 
+ * then below routes will never match
+ * app.get('/about/contact', () => { ... });
+ * app.get('/about/other', () => { ... });
  */
-app.get('/', (req, res) => {
-    /**
-     * if Handlebars engine is not used (for each page):
-     * res.type('text/plain'); - set Content-Type heading
-     * res.send('Home page');
-     */
-    res.render('home');
-});
+app.get('/', handlers.home);
 
-app.get('/about', (req, res) => {
-    // send dynamically generated value
-    res.render('about', { cookie: cookies.getCookie() });
-});
+app.get('/about', handlers.about);
 
-app.get('/about/contact', (req, res) => {
-    res.render('contact');
-});
+app.get('/about/contact', handlers.contact);
 
 /**
  * not standard pages are handled by app.use (it adds middleware)
  * it handles request which do not match to any defined routes
  * if Handlebars engine is used we have to explicitly establish status
  */
-app.use((req, res) => {
-    res.status(404);
-    res.render('404');
-});
+app.use(handlers.notFound);
 
-app.use((err, req, res, next) => {
-    console.error(err.message);
-    res.status(500);
-    res.render('500');
-});
+app.use(handlers.serverError);
 
 app.listen(port, () => console.log(
     `Express app is running: http://localhost:${port}`
